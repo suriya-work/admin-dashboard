bf436751aedbb58cf33f9923afb029fc9058626a
suria babaei <suriyalifework60@gmail.com>
2023-11-18 19:35:15 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './features/identity/components/login'
+
+const App = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default App
+
